6da4f603b566156d28aa50dab5bd4b31439af277
Sana-Malak <94910194+Sana-Malak@users.noreply.github.com>
2023-02-21 00:10:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+objectDetector= "";
+
 img="";
+objects = [];
 status="";
 
 function preload(){
@@ -12,20 +15,6 @@ function setup() {
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
-function draw() {
-    image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Bed", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350 );
-
-    fill("#FF0000");
-    text("Pillow", 320, 120)
-    noFill();
-    stroke("#FF0000")
-    rect(300, 90, 270, 170 );
-}
 function modelLoaded() {
     console.log("Model Loaded!")
     status = true;
@@ -37,4 +26,25 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status !="")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x+ 15, objects[i].y+ 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
